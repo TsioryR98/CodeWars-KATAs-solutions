c18b2b9e00d5914b03c5ed509d9a203f6e860df7
TsioryR98 <hei.tsiory.2@gmail.com>
2024-07-06 15:38:14 +0200

    feat: add from EndeavourOs

---
+++ sortTheOdds.js
@@ -0,0 +1,27 @@
+/*
+[7, 1]  =>  [1, 7]
+[5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+ */
+
+let array1 = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+
+function sortArray(array) {
+ 
+    let odds = [];
+    let result = [];
+    for (let index = 0; index < array.length; index++) {
+        if (array[i] % 2 != 0) {
+            
+        } else {
+            
+        }
+        
+    }
+        
+    //return odds.sort((a,b) => a-b);
+    return even;
+}
+
+console.log(sortArray(array1))
